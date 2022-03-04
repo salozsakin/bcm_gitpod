34e845fd71fbf3661a92458ae33ba5928fc2662e
Alba Leiva <albaleiva@MacBook-Pro-de-Alba.local>
2022-03-04 13:37:07 +0100

    BC-40: add custom controls for the carousel

--- web/themes/custom/brucecastle/components/components-overrides/carousel/carousel.js
+++ web/themes/custom/brucecastle/components/components-overrides/carousel/carousel.js
@@ -1,19 +1,52 @@
-(function (Drupal, $) {
+((Drupal, $) => {
   Drupal.behaviors.homeCarousel = {
-    attach: function attach(context) {
-      const sliderContainer = $(
+    attach(context) {
+      const $sliderContainer = $(
         '.paragraph--type--home-page-carousel .field--name-field-background-image',
         context,
       );
-      if (sliderContainer.length > 0) {
-        $(sliderContainer).slick({
+      const $navigationContainer = $(
+        '.home-page-carousel__custom-nav',
+        context,
+      );
+      const $playButton = $navigationContainer.children(
+        '.button--play--custom',
+      );
+      const $pauseButton = $navigationContainer.children(
+        '.button--pause--custom',
+      );
+      // Initialize slick
+      if ($sliderContainer.length > 0) {
+        $sliderContainer.slick({
           dots: true,
           slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
+          fade: true,
+          autoplay: false,
+          autoplaySpeed: 3000,
           arrowsPlacement: 'afterSlides',
         });
       }
+      // Bring slick dots within the custom slider controls
+      const slickDots = $sliderContainer.find('.slick-dots');
+      $navigationContainer.append(slickDots);
+      // Set custom next and prev controls to trigger default slick controls
+      $navigationContainer.children('.button--prev--custom').on('click', () => {
+        $sliderContainer.find('.slick-prev').trigger('click');
+      });
+      $navigationContainer.children('.button--next--custom').on('click', () => {
+        $sliderContainer.find('.slick-next').trigger('click');
+      });
+      // Start and pause autoplay on click
+      $playButton.on('click', () => {
+        $sliderContainer.slick('slickPlay');
+        $pauseButton.removeClass('button--hidden');
+        $playButton.addClass('button--hidden');
+      });
+      $pauseButton.on('click', () => {
+        $sliderContainer.slick('slickPause');
+        $playButton.removeClass('button--hidden');
+        $pauseButton.addClass('button--hidden');
+      });
     },
   };
 })(Drupal, jQuery);
