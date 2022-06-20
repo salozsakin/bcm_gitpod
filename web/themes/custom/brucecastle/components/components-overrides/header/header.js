e1833ed4ca219b2e248a2ba4b2e364e28dd4b7b7
Lydia Wood <lydia@zoocha.com>
2022-06-20 09:54:46 +0100

    BC-9: icon js and scss initial

---
+++ web/themes/custom/brucecastle/components/components-overrides/header/header.js
@@ -0,0 +1,11 @@
+console.log('lydia');
+
+const searchBox = document.getElementById('block-brucecastle-localgov-sitewide-search-block-scarfolk');
+
+console.log(searchBox);
+
+const searchIcon = document.createElement('img');
+// searchIcon.setAttribute("src", "/images/icons/search-icon.svg");
+searchIcon.setAttribute("src", "replace");
+searchBox.appendChild(searchIcon);
+console.log(searchIcon);
