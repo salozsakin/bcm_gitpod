34761d2496af3fd0834cd794341668d08ab84782
Lydia Wood <lydia@zoocha.com>
2022-06-20 15:17:36 +0100

    BC-9: js toggle and scss

--- web/themes/custom/brucecastle/components/components-overrides/header/header.js
+++ web/themes/custom/brucecastle/components/components-overrides/header/header.js
@@ -1,5 +1,26 @@
 const searchBox = document.getElementById('block-brucecastle-localgov-sitewide-search-block-scarfolk');
+const searchButton = document.createElement('button');
 const searchIcon = document.createElement('img');
+const searchTextBox = document.getElementById('views-exposed-form-localgov-sitewide-search-sitewide-search-page-block');
 searchIcon.setAttribute("src", "/themes/custom/brucecastle/images/icons/search-icon.svg");
 searchIcon.setAttribute("class", "sitewide-search-block__search-icon");
-searchBox.appendChild(searchIcon);
+searchButton.setAttribute("class", "sitewide-search-block__search-button");
+searchButton.appendChild(searchIcon);
+searchBox.appendChild(searchButton);
+
+searchButton.addEventListener('click', e => {
+  if (searchTextBox.style.display && searchTextBox.style.display !== 'none') {
+    searchTextBox.style.display = 'none';
+  } else {
+    searchTextBox.style.display = 'flex';
+  }
+});
+
+window.addEventListener('resize', () => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 800) {
+    searchTextBox.style.display = 'none';
+  } else {
+    searchTextBox.style.display = 'flex';
+  }
+});
