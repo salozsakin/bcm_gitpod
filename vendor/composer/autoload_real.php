6471ee388b3d793eaae550dc245a749afe7ac9df
Viktor Tamas <vtamas@ixis.co.uk>
2022-06-24 16:31:28 +0100

    [#19144] security update core to 9.3.17

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,10 +22,8 @@ class ComposerAutoloaderInitc320fa488793ac4f82ff468e7717fa3f
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitc320fa488793ac4f82ff468e7717fa3f', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc320fa488793ac4f82ff468e7717fa3f', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
@@ -34,7 +32,7 @@ class ComposerAutoloaderInitc320fa488793ac4f82ff468e7717fa3f
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitc320fa488793ac4f82ff468e7717fa3f::getInitializer($loader));
         } else {
@@ -69,16 +67,11 @@ class ComposerAutoloaderInitc320fa488793ac4f82ff468e7717fa3f
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequirec320fa488793ac4f82ff468e7717fa3f($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
