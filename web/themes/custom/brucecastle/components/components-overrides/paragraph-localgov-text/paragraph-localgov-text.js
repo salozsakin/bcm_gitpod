8ca83bb3da7ca8eaed85c2d46c29b22a040a50b0
lincoln <holincolnly@gmail.com>
2022-07-29 12:28:03 +0100

    BC-54: Update javascript + php options

--- web/themes/custom/brucecastle/components/components-overrides/paragraph-localgov-text/paragraph-localgov-text.js
+++ web/themes/custom/brucecastle/components/components-overrides/paragraph-localgov-text/paragraph-localgov-text.js
@@ -1,19 +1,39 @@
-((Drupal, $) => {
+((Drupal) => {
   Drupal.behaviors.paragraphText = {
     attach(context) {
-      console.log('hola');
-      const wysiwyg = $('.field--name-localgov-text');
+      const wysiwyg = context.querySelectorAll('.field--name-localgov-text');
 
-      console.log(wysiwyg.find('[data-hspace]').getAttribute());
+      wysiwyg.forEach((element) => {
+        const mediaWithVspace = element.querySelectorAll(['[data-vspace]']);
+        mediaWithVspace.forEach((mediaElement) => {
+          mediaElement.style.marginTop = `${mediaElement.getAttribute(
+            'data-vspace',
+          )}px`;
+          mediaElement.style.marginBottom = `${mediaElement.getAttribute(
+            'data-vspace',
+          )}px`;
+        });
 
-      // data-hspace
-      // if (wysiwyg.find('[data-hspace]')){
-      //   console.log($(this).getAttribute('[data-hspace]'));
-      // }
+        const mediaWithHspace = element.querySelectorAll(['[data-hspace]']);
+        mediaWithHspace.forEach((mediaElement) => {
+          mediaElement.style.marginRight = `${mediaElement.getAttribute(
+            'data-hspace',
+          )}px`;
+          mediaElement.style.marginLeft = `${mediaElement.getAttribute(
+            'data-hspace',
+          )}px`;
+        });
 
-      // data-vspace
-
-      // data-border
+        const mediaWithBorder = element.querySelectorAll(['[data-border]']);
+        mediaWithBorder.forEach((mediaElement) => {
+          const imageElement = mediaElement.querySelector('img');
+          if (imageElement) {
+            imageElement.style.border = mediaElement.getAttribute(
+              'data-border',
+            );
+          }
+        });
+      });
     },
   };
-})(Drupal, jQuery);
+})(Drupal);
